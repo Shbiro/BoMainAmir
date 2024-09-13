89122b19d57632d893655b0cb692e97d200ba906
Amir Atias Hori <amiratiashuri@gmail.com>
2024-09-13 15:14:08 +0300

    שינוי קובץ Readme

--- readme.py
+++ readme.py
@@ -3,8 +3,13 @@
 ############################ הרצת שרת pm2 ##################
 #pm2 start Server.py --interpreter "C:\Users\USER\AppData\Local\Programs\Python\Python312\python.exe"
 
+
+
 #התחברות ל ssh
-# ssh -i "amir.pem" ec2-user@51.20.244.254
+#ssh -i "Amir.pem" ec2-user@ec2-51-20-244-254.eu-north-1.compute.amazonaws.com
+
+#העלה לפרודקשן
+#scp -i C:\Users\USER\source\repos\amir.pem -r build C:\Users\USER\source\repos\BoMainAmir\Server.js ec2-user@ec2-51-20-244-254.eu-north-1.compute.amazonaws.com:~/BoMainAmir
 
 
 
